640b9824d0c0b3b5d75f8657d21bff29ec99d8e2
Alexandr Dylevich <dylevich220885@gmail.com>
2023-06-11 17:28:09 +0300

    Вместо обычной кнопки используется компонента Button (Part 2 with с исправлениями после отправки)

--- src/contacts/Contacts.tsx
+++ src/contacts/Contacts.tsx
@@ -2,9 +2,11 @@ import React from 'react';
 import style from './Contacts.module.scss';
 //import style from './Contacts2.module.css';
 import styleContainer from '../common/styles/Container.module.scss';
-import {Title} from '../common/components/title/Title';
+import {Title} from 'common/components/title/Title';
+import Button from 'common/components/button/Button';
 
 export const Contacts = () => {
+    //TODO стилизовать поля в форме (замечание от Светланы)
     return (
         <div className={style.contactsBlock}>
             <div className={`${styleContainer.container} ${style.contactsContainer}`}>
@@ -14,8 +16,10 @@ export const Contacts = () => {
                     <input className={style.fields} type="text"/>
                     <input className={style.fields} type="text"/>
                     <textarea className={`${style.textArea} ${style.fields}`}></textarea>
+
                     <div className={style.buttonContainer}>
-                        <button className={style.button}>Send</button>
+                        {/*<button className={style.button}>Send</button>*/}
+                        <Button buttonValue={'Send'}/>
                     </div>
                 </form>
             </div>
