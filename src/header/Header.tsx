61cbb7c1da485f0f446c4fa59fe82f4139f6a233
Alexandr Dylevich <dylevich220885@gmail.com>
2023-06-12 23:16:21 +0300

    добавил отрисовку <BurgerNav/> (Part 3)

--- src/header/Header.tsx
+++ src/header/Header.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import style from './Header.module.scss';
 import styleContainer from '../common/styles/Container.module.scss';
-import {Nav} from '../nav/Nav';
+import {Nav} from './nav/Nav';
+import {BurgerNav} from 'header/burgerNav/BurgerNav';
 
 export const Header = () => {
-    return (
-        <div className={`${style.header}`}>
-            <Nav/>
-        </div>
-    );
+	return (
+		<div className={`${style.header}`}>
+			<Nav/>
+			<BurgerNav/>
+		</div>
+	);
 }
 
